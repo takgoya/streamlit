19e59861a2f47d3b2601842e04c836721c986dc1
takgoya <javier.goya@gmail.com>
2022-12-04 21:52:30 +0100

    updated filters

--- image_filters/image_filters.py
+++ image_filters/image_filters.py
@@ -25,3 +25,73 @@ def sepia_filter(img):
 
     return img_sepia
 
+@st.cache
+def vignette_filter(img, level=2):
+    height, width = img.shape[:2]
+
+    # Generate vignette mask using Gaussian kernels
+    X_resultant_kernel = cv2.getGaussianKernel(width, width/level)
+    Y_resultant_kernel = cv2.getGaussianKernel(height, height/level)
+
+    # Generate resultant_kernel matrix
+    kernel = Y_resultant_kernel * X_resultant_kernel.T
+    mask = kernel / kernel.max()
+
+    img_vignette = img.copy()
+
+    # Applying the mask to each channel in the input image
+    for i in range(3):
+        img_vignette[:,:,i] = img_vignette[:,:,i] * mask 
+
+    return img_vignette
+
+@st.cache
+def edge_detection_filter(img, lower_thresh, upper_thresh):
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img_blurred = cv2.GaussianBlur(img_gray, (5,5), 0, 0)
+    img_thresh = cv2.Canny(img_blurred, lower_thresh, upper_thresh)
+    
+    return img_thresh
+
+@st.cache
+def brightness_filter(img, brigthness):
+    img_bright = cv2.convertScaleAbs(img, beta=brigthness)
+    return img_bright
+
+@st.cache
+def contrast_filter(img, contrast):
+    img_contrast = cv2.convertScaleAbs(img, alpha=contrast)
+    return img_contrast
+
+@st.cache
+def embossed_edges_filter(img):
+    kernel = np.array([[0, -3, -3],
+                       [3, 0, -3],
+                       [3, 3, 0]])
+
+    img_embossed = cv2.filter2D(img, -1, kernel=kernel)
+
+    return img_embossed
+
+@st.cache
+def outline_filter(img, k):
+    k = max(k, 9)
+    kernel = np.array([[-1, -1, -1],
+                       [-1, k, -1],
+                       [-1, -1, -1]])
+
+    img_outline = cv2.filter2D(img, ddepth=-1, kernel=kernel)
+
+    return img_outline
+
+@st.cache
+def pencil_sketch_filter(img, kernel=5):
+    img_blur = cv2.GaussianBlur(img, (kernel, kernel), 0, 0)
+    img_sketch, _ = cv2.pencilSketch(img_blur)
+    return img_sketch
+
+@st.cache
+def stylization_filter(img):
+    img_blur = cv2.GaussianBlur(img, (5, 5), 0, 0)
+    img_style = cv2.stylization(img_blur, sigma_s=40, sigma_r=0.1)
+    return img_style
--- image_filters/image_filters_streamlit_app.py
+++ image_filters/image_filters_streamlit_app.py
@@ -1,11 +1,20 @@
-import os
+import io
+import base64
 
 import cv2
 import numpy as np
 import streamlit as st
+from PIL import Image
 
-from image_filters import black_white_filter, sepia_filter
+from image_filters import *
 
+# Generating a link to download a particular image file
+def get_image_download_link(img, filename, text):
+    buffered = io.BytesIO()
+    img.save(buffered, format='JPEG')
+    img_str = base64.b64encode(buffered.getvalue()).decode()
+    href = f'<a href="data:file/txt;base64,{img_str}" download="{filename}">{text}</a>'
+    return href
 
 # Set a title
 st.title('Image Filters')
@@ -30,7 +39,15 @@ if img_uploaded is not None:
     option = st.selectbox('Select a filter to apply:', 
                           ('None', 
                           'Black & White',
-                          'Sepia / Vintage'))
+                          'Sepia / Vintage',
+                          'Vignette Effect',
+                          'Edge Detection',
+                          'Brightness',
+                          'Contrast',
+                          'Embossed Edges',
+                          'Outline Filter',
+                          'Pencil Sketch',
+                          'Stylization Filter') )
 
     # Flag for showing the image
     output_display = True
@@ -46,8 +63,44 @@ if img_uploaded is not None:
         output_color = 'GRAY'
     elif option == 'Sepia / Vintage':
         img_output = sepia_filter(img)
+    elif option == 'Vignette Effect':
+        level = st.slider('level', min_value=1, max_value=5, value=2)
+        img_output = vignette_filter(img, level)
+    elif option == 'Edge Detection':
+        lower_thresh = st.slider('lower_thresh', min_value=10, max_value=150, step=5, value=50 )
+        upper_thresh = st.slider('thrupper_threshesh2', min_value=lower_thresh, max_value=255, step=5, value=200)
+        img_output = edge_detection_filter(img, lower_thresh, upper_thresh)
+        output_color = 'GRAY'
+    elif option == 'Brightness':
+        brightness = st.slider('brightness', min_value=-50, max_value=50, step=10, value=0)
+        img_output = brightness_filter(img, brightness)
+    elif option == 'Contrast':
+        contrast = st.slider('contrast', min_value=0.2, max_value=2.0, step=0.2, value=1.0)
+        img_output = contrast_filter(img, contrast)
+    elif option == 'Embossed Edges':
+        img_output = embossed_edges_filter(img)
+    elif option == 'Outline Filter':
+        k = st.slider('k', min_value=9, max_value=12, step=1, value=9)
+        img_output = outline_filter(img, k)
+    elif option == 'Pencil Sketch':
+        kernel = st.slider('kernel', min_value=1, max_value=9, step=2, value=5)
+        img_output = pencil_sketch_filter(img)
+        output_color = 'GRAY'
+    elif option == 'Stylization Filter':
+        img_output = stylization_filter(img)
+
 
     with output_col:
         if output_display == True:
             st.header(option)
             st.image(img_output, channels=output_color, use_column_width=True)
+
+            # fromarray convert cv2 image into PIL format for saving it using download link.
+            if output_color == 'BGR':
+                result = Image.fromarray(img_output[:,:,::-1])
+            else:
+                result = Image.fromarray(img_output)
+            # Display link.
+            st.markdown(get_image_download_link(result,'output.png','Download '+'Output'),
+                        unsafe_allow_html=True)
+
